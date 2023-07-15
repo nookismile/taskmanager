8c939dd28aed76fb3a69b0ecfec9efdee5a7a60e
nookismile <stina.novickova@yandex.ru>
2023-07-15 15:39:39 +0300

    add files

---
+++ src/view/task-view.js
@@ -0,0 +1,58 @@
+import {createElement} from '../render.js';
+
+function createTaskTemplate() {
+  return (
+    `<article class="card card--black">
+            <div class="card__form">
+              <div class="card__inner">
+                <div class="card__control">
+                  <button type="button" class="card__btn card__btn--edit">
+                    edit
+                  </button>
+                  <button type="button" class="card__btn card__btn--archive">
+                    archive
+                  </button>
+                  <button
+                    type="button"
+                    class="card__btn card__btn--favorites"
+                  >
+                    favorites
+                  </button>
+                </div>
+
+                <div class="card__color-bar">
+                  <svg class="card__color-bar-wave" width="100%" height="10">
+                    <use xlink:href="#wave"></use>
+                  </svg>
+                </div>
+
+                <div class="card__textarea-wrap">
+                  <p class="card__text">Example task without date.</p>
+                </div>
+
+                <div class="card__settings">
+                  <div class="card__details"></div>
+                </div>
+              </div>
+            </div>
+          </article>`
+  );
+}
+
+export default class TaskView {
+  getTemplate() {
+    return createTaskTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
